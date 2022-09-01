0dae1f0dceeee7fd32e314422f13fc55569da31c
Kuromi <Kuromi@DESKTOP-Q52Q2A7>
2022-09-01 10:57:51 +0200

    Add project files.

---
+++ CheeseBurgerJustToEat/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheeseBurgerJustToEat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tast inn Temperatur");
+            int grader = Convert.ToInt32(Console.ReadLine());
+
+            if (grader > 0)
+
+            {
+
+                Console.WriteLine("PlusGrader");
+            }
+
+            else if (grader == 0)
+
+            {
+                Console.WriteLine("Null Grader");
+            }
+
+            else
+
+            {
+
+                Console.WriteLine("MinusGrader");
+            }
+
+        }
+    }
+}
